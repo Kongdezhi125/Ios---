90740d94a912b581b43fb8b598e7b22dfef0960e
Kdz <64460590+Kongdezhi125@users.noreply.github.com>
2021-05-27 09:29:55 +0800

    图片上下文点击出现菜单

--- 多种类型菜单/多种类型菜单/ViewController.swift
+++ 多种类型菜单/多种类型菜单/ViewController.swift
@@ -9,10 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgs: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "menu", image: UIImage(systemName: "phone"), menu: addmenus())
+        
+        let context = UIContextMenuInteraction(delegate: self)
+        imgs.addInteraction(context)
     }
 
     func addmenus() -> UIMenu {
@@ -30,4 +34,22 @@ class ViewController: UIViewController {
         return menu
     }
 }
-
+extension ViewController:UIContextMenuInteractionDelegate{
+    func contextMenuInteraction(_ interaction: UIContextMenuInteraction, configurationForMenuAtLocation location: CGPoint) -> UIContextMenuConfiguration? {
+        let action1 = UIAction(title: "扫一扫", image: UIImage(systemName: "qrcode.viewfinder")) { (UIAction) in
+            print("点击扫一扫")
+        }
+        let action2 = UIAction(title: "看一看", image: UIImage(systemName: "person")) { (UIAction) in
+            print("点击看一看")
+        }
+        let action3 = UIAction(title: "爱心", image: UIImage(systemName: "heart")) { (UIAction) in
+            print("点击爱心")
+        }
+        let configuration = UIContextMenuConfiguration(identifier: nil, previewProvider: nil) { _ in
+            UIMenu(children: [action1,action2,action3])
+        }
+        return configuration
+    }
+    
+    
+}
